ab23e3d09fa6a94d01a68917fdac3b2bc79dc243
minagishl <mvxd568.co@icloud.com>
2025-03-19 21:30:52 +0900

    feat: add status message

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,21 @@
-import { type Client, Events } from "discord.js";
+import { ActivityType, type Client, Events } from "discord.js";
 
 export default {
   name: Events.ClientReady,
   once: true,
   async execute(client: Client): Promise<void> {
+    const setActivityStatus = (): void => {
+      client.user?.setActivity("Get started using /create!", {
+        type: ActivityType.Custom,
+      });
+    };
+
+    // Initial setting
+    setActivityStatus();
+
+    // Set activity every 12 hours (12 * 60 * 60 * 1000 ms)
+    setInterval(setActivityStatus, 43200000);
+
     // Log the username of the client when it is ready.
     console.log(`Logged in as ${client.user?.username}!`);
   },
